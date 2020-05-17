b64f948304444b4cac4732ee2cc94816b840a47e
Alan Grubb <atgrubb@gmail.com>
2020-05-17 09:51:42 -0700

    Refactor and clean up area page with enums

--- pages/area_page.py
+++ pages/area_page.py
@@ -1,95 +1,75 @@
-from selenium.common.by import By
+from enum import Enum
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 from utilities.web_driver import WebDriver
 
 
 class AreaPage(WebDriver):
-    def __init__(self):
+    def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
-    # LOCATORS
-    _area_header = "//div[@class='regular-area']/h2[@class='area']"
+    class Locators(Enum):
+            AREA_HEADER = ("//div[@class='regular-area']/h2[@class='area']", By.XPATH)
+            SEARCH_CRAIGSLIST = ("query", By.ID)
 
-    # housing locators
-    _housing_header = "//a[@class='hhh']"
-    _apts_housing = "//a[@class='apa']"
-    _housing_swap = "//a[@class='swp']"
-    _housing_wanted = "//a[@class='hsw']"
-    _office_commercial = "//a[@class='off']"
-    _parking_storage = "//a[@class='prk']"
-    _real_estate_for_sale = "//a[@class='rea']"
-    _rooms_shared = "//a[@class='roo']"
-    _rooms_wanted = "//a[@class='sha']"
-    _sublets_temporary = "//a[@class='sub']"
-    _vacation_rentals = "//a[@class='vac']"
+    class HousingLocators(Enum):
+        HOUSING_HEADER = ("//a[@class='hhh']", By.XPATH)
+        APTS_HOUSING = ("//a[@class='apa']", By.XPATH)
+        HOUSING_SWAP = ("//a[@class='swp']", By.XPATH)
+        HOUSING_WANTED = ("//a[@class='hsw']", By.XPATH)
+        OFFICE_COMMERCIAL = ("//a[@class='off']", By.XPATH)
+        PARKING_STORAGE = ("//a[@class='prk']", By.XPATH)
+        REAL_ESTATE_FOR_SALE = ("//a[@class='rea']", By.XPATH)
+        ROOMS_SHARED = ("//a[@class='roo']", By.XPATH)
+        ROOMS_WANTED = ("//a[@class='sha']", By.XPATH)
+        SUBLETS_TEMPORARY = ("//a[@class='sub']", By.XPATH)
+        VACATION_RENTALS = ("//a[@class='vac']", By.XPATH)
 
-    # jobs locators
-    _jobs_header = "//a[@class='jjj']"
-    _accounting_finance = "//a[@class='acc']"
-    _admin_office = "//a[@class='ofc']"
-    _arch_engineering = "//a[@class='egr']"
-    _art_media_design = "//a[@class='med']"
-    _biotech_science = "//a[@class='sci']"
-    _business_mgmt = "//a[@class='bus']"
-    _customer_service = "//a[@class='csr']"
-    _education = "//a[@class='edu']"
-    _etc_misc = "//a[@class='etc']"
-    _food_bev_hosp = "//a[@class='fbh']"
-    _general_labor = "//a[@class='lab']"
-    _government = "//a[@class='gov']"
-    _human_resources = "//a[@class='hum']"
-    _legal_paralegal = "//a[@class='lgl']"
-    _manufacturing = "//a[@class='mnu']"
-    _marketing_pr_ad = "//a[@class='mar']"
-    _medical_health = "//a[@class='hea']"
-    _nonprofit_sector = "//a[@class='npo']"
-    _real_estate = "//a[@class='rej']"
-    _retail_wholesale = "//a[@class='ret']"
-    _sales_biz_dev = "//a[@class='sls']"
-    _salon_spa_fitness = "//a[@class='spa']"
-    _security = "//a[@class='sec']"
-    _skilled_trade_craft = "//a[@class='trd']"
-    _software_qa_dba = "//a[@class='sof']"
-    _systems_network = "//a[@class='sad']"
-    _technical_support = "//a[@class='tch']"
-    _transport = "//a[@class='trp']"
-    _tv_film_video = "//a[@class='tfr']"
-    _web_info_design = "//a[@class='web']"
-    _writing_editing = "//a[@class='wri']"
-
-    # GETTERS
-    # housing getters
-    def get_housing_anchor(self):
-        return self.driver.find_element(By.XPATH, self._housing_header)
-
-    def get_apts_housing_anchor(self):
-        return self.driver.find_element(By.XPATH, self._apts_housing)
-
-    def get_housing_swap_anchor(self):
-        return self.driver.find_element(By.XPATH, self._housing_swap)
-
-    def get_housing_wanted_anchor(self):
-        return self.driver.find_element(By.XPATH, self._housing_wanted)
-
-    def get_office_commercial_anchor(self):
-        return self.driver.find_element(By.XPATH, self._office_commercial)
-
-    def get_parking_storage_anchor(self):
-        return self.driver.find_element(By.XPATH, self._parking_storage)
-
-    def get_real_estate_for_sale_anchor(self):
-        return self.driver.find_element(By.XPATH, self._real_estate_for_sale)
-
-    def get_rooms_shared_anchor(self):
-        return self.driver.find_element(By.XPATH, self._rooms_shared)
-
-    def get_rooms_wanted_anchor(self):
-        return self.driver.find_element(By.XPATH, self._rooms_wanted)
-
-    def get_sublets_temporary_anchor(self):
-        return self.driver.find_element(By.XPATH, self._sublets_temporary)
-
-    def get_vacation_rentals_anchor(self):
-        return self.driver.find_element(By.XPATH, self._vacation_rentals)
+    class JobsLocators(Enum):
+        JOBS_HEADER = ("//a[@class='jjj']", By.XPATH)
+        ACCOUNTING_FINANCE = ("//a[@class='acc']", By.XPATH)
+        ADMIN_OFFICE = ("//a[@class='ofc']", By.XPATH)
+        ARCH_ENGINEERING = ("//a[@class='egr']", By.XPATH)
+        ART_MEDIA_DESIGN = ("//a[@class='med']", By.XPATH)
+        BIOTECH_SCIENCE = ("//a[@class='sci']", By.XPATH)
+        BUSINESS_MGMT = ("//a[@class='bus']", By.XPATH)
+        CUSTOMER_SERVICE = ("//a[@class='csr']", By.XPATH)
+        EDUCATION = ("//a[@class='edu']", By.XPATH)
+        ETC_MISC = ("//a[@class='etc']", By.XPATH)
+        FOOD_BEV_HOSP = ("//a[@class='fbh']", By.XPATH)
+        GENERAL_LABOR = ("//a[@class='lab']", By.XPATH)
+        GOVERNMENT = ("//a[@class='gov']", By.XPATH)
+        HUMAN_RESOURCES = ("//a[@class='hum']", By.XPATH)
+        LEGAL_PARALEGAL = ("//a[@class='lgl']", By.XPATH)
+        MANUFACTURING = ("//a[@class='mnu']", By.XPATH)
+        MARKETING_PR_AD = ("//a[@class='mar']", By.XPATH)
+        MEDICAL_HEALTH = ("//a[@class='hea']", By.XPATH)
+        NONPROFIT_SECTOR = ("//a[@class='npo']", By.XPATH)
+        REAL_ESTATE = ("//a[@class='rej']", By.XPATH)
+        RETAIL_WHOLESALE = ("//a[@class='ret']", By.XPATH)
+        SALES_BIZ_DEV = ("//a[@class='sls']", By.XPATH)
+        SALON_SPA_FITNESS = ("//a[@class='spa']", By.XPATH)
+        SECURITY = ("//a[@class='sec']", By.XPATH)
+        SKILLED_TRADE_CRAFT = ("//a[@class='trd']", By.XPATH)
+        SOFTWARE_QA_DBA = ("//a[@class='sof']", By.XPATH)
+        SYSTEMS_NETWORK = ("//a[@class='sad']", By.XPATH)
+        TECHNICAL_SUPPORT = ("//a[@class='tch']", By.XPATH)
+        TRANSPORT = ("//a[@class='trp']", By.XPATH)
+        TV_FILM_VIDEO = ("//a[@class='tfr']", By.XPATH)
+        WEB_INFO_DESIGN = ("//a[@class='web']", By.XPATH)
+        WRITING_EDITING = ("//a[@class='wri']", By.XPATH)
 
     # ACTIONS
+    def click(self, locator=Locators):
+        self.click_element(locator.value, locator.value[1])
+
+    def click_housing_locator(self, locator=HousingLocators):
+        self.click_element(locator.value, locator.value[1])
+
+    def click_jobs_locator(self, locator=JobsLocators):
+        self.click_element(locator.value, locator.value[1])
+
+    def send_keys_search_craigslist(self):
+        self.send_keys_element(self.Locators.SEARCH_CRAIGSLIST.value, self.Locators.SEARCH_CRAIGSLIST.value[1])
+
