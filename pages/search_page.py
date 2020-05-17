8258272130f344a1d2c4ce930070a6d4a4be4531
Alan Grubb <atgrubb@gmail.com>
2020-05-17 14:42:08 -0700

    Refactor search page to remove enums and simplify unpacking of tuples

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,41 +1,49 @@
-from enum import Enum
 from selenium.webdriver.common.by import By
-from utilities.web_driver import WebDriver
+from selenium.webdriver.support.select import Select
 
-class SearchPage(WebDriver):
+class SearchPage:
     def __init__(self, driver):
-        super().__init__(driver)
         self.driver = driver
 
-    # Locators
+    # LOCATORS
 
-    class OptionsLocators(Enum):
-        AREA_OPTIONS = ("areaAbb", By.ID)
-        CATEGORIES_OPTIONS = ("catAbb", By.ID)
-        SUBCATEGORIES_OPTIONS = ("subcatAbb", By.ID)
+    class OptionsLocators:
+        AREA_OPTIONS = (By.ID, "areaAbb")
+        CATEGORIES_OPTIONS = (By.ID, "catAbb")
+        SUBCATEGORIES_OPTIONS = (By.ID, "subcatAbb")
 
-    class CategoriesOptionsLocators(Enum):
-        COMMUNITY = ("//select[@id='catAbb']/option[@value='ccc']", By.XPATH)
-        EVENTS = ("//select[@id='catAbb']/option[@value='eee']", By.XPATH)
-        FOR_SALE = ("//select[@id='catAbb']/option[@value='sss']", By.XPATH)
-        GIGS = ("//select[@id='catAbb']/option[@value='ggg']", By.XPATH)
-        HOUSING = ("//select[@id='catAbb']/option[@value='hhh']", By.XPATH)
-        JOBS = ("//select[@id='catAbb']/option[@value='jjj']", By.XPATH)
-        RESUMES = ("//select[@id='catAbb']/option[@value='rrr']", By.XPATH)
-        SERVICES = ("//select[@id='catAbb']/option[@value='bbb']", By.XPATH)
+    class CategoriesOptionsLocators:
+        COMMUNITY = (By.XPATH, "//select[@id='catAbb']/option[@value='ccc']")
+        EVENTS = (By.XPATH, "//select[@id='catAbb']/option[@value='eee']")
+        FOR_SALE = (By.XPATH, "//select[@id='catAbb']/option[@value='sss']")
+        GIGS = (By.XPATH, "//select[@id='catAbb']/option[@value='ggg']")
+        HOUSING = (By.XPATH, "//select[@id='catAbb']/option[@value='hhh']")
+        JOBS = (By.XPATH, "//select[@id='catAbb']/option[@value='jjj']")
+        RESUMES = (By.XPATH, "//select[@id='catAbb']/option[@value='rrr']")
+        SERVICES = (By.XPATH, "//select[@id='catAbb']/option[@value='bbb']")
 
-    class HousingSubCategoriesLocators(Enum):
-        ALL = ("//select[@id='subcatAbb']/option[@value='hhh']", By.XPATH)
-        APTS_HOUSING = ("//select[@id='subcatAbb']/option[@value='apa']", By.XPATH)
-        HOUSING_SWAP = ("//select[@id='subcatAbb']/option[@value='swp']]", By.XPATH)
-        OFFICE_COMMERCIAL = ("//select[@id='subcatAbb']/option[@value='off']", By.XPATH)
-        PARKING_STORAGE = ("//select[@id='subcatAbb']/option[@value='prk']", By.XPATH)
-        REAL_ESTATE_BY_BROKER = ("//select[@id='subcatAbb']/option[@value='reb']", By.XPATH)
-        REAL_ESTATE_BY_OWNER = ("//select[@id='subcatAbb']/option[@value='reo']", By.XPATH)
-        ROOMS_SHARED = ("//select[@id='subcatAbb']/option[@value='roo']", By.XPATH)
-        SUBLETS_TEMPORARY = ("//select[@id='subcatAbb']/option[@value='sub']", By.XPATH)
-        VACATION_RENTALS = ("//select[@id='subcatAbb']/option[@value='vac']", By.XPATH)
-        WANTED_APTS = ("//select[@id='subcatAbb']/option[@value='hou']", By.XPATH)
-        WANTED_REAL_ESTATE = ("//select[@id='subcatAbb']/option[@value='rew']", By.XPATH)
-        WANTED_ROOM_SHARE = ("//select[@id='subcatAbb']/option[@value='sha']", By.XPATH)
-        WANTED_SUBLET_TEMP = ("//select[@id='subcatAbb']/option[@value='sbw']", By.XPATH)
+    class HousingSubCategoriesLocators:
+        ALL = (By.XPATH, "//select[@id='subcatAbb']/option[@value='hhh']")
+        APTS_HOUSING = (By.XPATH, "//select[@id='subcatAbb']/option[@value='apa']")
+        HOUSING_SWAP = (By.XPATH, "//select[@id='subcatAbb']/option[@value='swp']]")
+        OFFICE_COMMERCIAL = (By.XPATH, "//select[@id='subcatAbb']/option[@value='off']")
+        PARKING_STORAGE = (By.XPATH, "//select[@id='subcatAbb']/option[@value='prk']")
+        REAL_ESTATE_BY_BROKER = (By.XPATH, "//select[@id='subcatAbb']/option[@value='reb']")
+        REAL_ESTATE_BY_OWNER = (By.XPATH, "//select[@id='subcatAbb']/option[@value='reo']")
+        ROOMS_SHARED = (By.XPATH, "//select[@id='subcatAbb']/option[@value='roo']")
+        SUBLETS_TEMPORARY = (By.XPATH, "//select[@id='subcatAbb']/option[@value='sub']")
+        VACATION_RENTALS = (By.XPATH, "//select[@id='subcatAbb']/option[@value='vac']")
+        WANTED_APTS = (By.XPATH, "//select[@id='subcatAbb']/option[@value='hou']")
+        WANTED_REAL_ESTATE = (By.XPATH, "//select[@id='subcatAbb']/option[@value='rew']")
+        WANTED_ROOM_SHARE = (By.XPATH, "//select[@id='subcatAbb']/option[@value='sha']")
+        WANTED_SUBLET_TEMP = (By.XPATH, "//select[@id='subcatAbb']/option[@value='sbw']")
+
+    # ACTIONS
+
+    def verify_category_option_selected(self, option_locator=CategoriesOptionsLocators.COMMUNITY):
+        category_locator = self.OptionsLocators.CATEGORIES_OPTIONS
+        category_element = self.driver.find_element(*category_locator)
+        selected_option = Select(category_element).first_selected_option
+        option_element = self.driver.find_element(*option_locator)
+        print(selected_option.text + " should be equal to " + option_element.text)
+        return selected_option.text == option_element.text
