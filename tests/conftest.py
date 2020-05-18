a0af764536960c8d82419e6b37b4716a4ab780ad
Alan Grubb <atgrubb@gmail.com>
2020-05-17 19:05:57 -0700

    Flesh out conftest file for pytest execution

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,21 +4,40 @@ from pages.area_page import AreaPage
 import uuid
 
 
-def get_driver(self, browser):
-    if browser.lower() == "iexplorer":
-        driver = webdriver.Ie()
-    elif browser.lower() == "firefox":
-        driver = webdriver.Firefox()
-    elif browser.lower() == "chrome":
-        driver = webdriver.Chrome()
-    elif browser.lower() == "safari":
-        driver = webdriver.Safari()
+@pytest.yield_fixture(scope="class")
+def setUpClass(request, browser_stack_enabled, browser, browser_version, os, os_version, name):
+    # Set default for BrowserStack enabled
+    if browser_stack_enabled is None:
+        browser_stack_enabled = False
+
+    if browser_stack_enabled:
+        browser_stack_url = 'https://alangrubb1:mqyCphmri491xmxqBxq3@hub-cloud.browserstack.com/wd/hub'
+        desired_capabilities = get_desired_capabilities(browser, browser_version, os, os_version, name)
+        driver = webdriver.Remote(
+            command_executor=browser_stack_url,
+            desired_capabilities=desired_capabilities
+        )
     else:
-        driver = webdriver.Chrome()
+        if str(browser).lower() == "iexplorer":
+            driver = webdriver.Ie()
+        elif str(browser).lower() == "firefox":
+            driver = webdriver.Firefox()
+        elif str(browser).lower() == "chrome":
+            driver = webdriver.Chrome()
+        elif str(browser).lower() == "safari":
+            driver = webdriver.Safari()
+        else:
+            driver = webdriver.Chrome()
 
-    driver.maximize_window()
+        driver.maximize_window()
 
-    return driver
+    area_page = AreaPage(driver)
+
+    if request.cls is not None:
+        request.cls.driver = driver
+
+    yield driver
+    driver.quit()
 
 
 def get_desired_capabilities(browser, browser_version, os, os_version, name):
@@ -45,52 +64,19 @@ def get_desired_capabilities(browser, browser_version, os, os_version, name):
 
     return desired_capabilities
 
-@pytest.yield_fixture()
-#@pytest.fixture()
-def setUp():
-    print("Running method level setUp")
-    yield
-    print("Running method level tearDown")
-
-@pytest.yield_fixture(scope="class")
-#@pytest.fixture(scope="class")
-def oneTimeSetUp(request, browser_stack_enabled, browser, browser_version, os, os_version, name):
-    # Set default for BrowserStack enabled
-    if browser_stack_enabled is None:
-        browser_stack_enabled = False
-
-    if browser_stack_enabled:
-        browser_stack_url = 'https://alangrubb1:mqyCphmri491xmxqBxq3@hub-cloud.browserstack.com/wd/hub'
-        desired_capabilities = get_desired_capabilities(browser, browser_version, os, os_version, name)
-        driver = webdriver.Remote(
-            command_executor=browser_stack_url,
-            desired_capabilities=desired_capabilities
-        )
-    else:
-        print("setting driver")
-        driver = get_driver(browser)
-        area_page = AreaPage(driver)
-        print("driver set")
-
-    if request.cls is not None:
-        request.cls.driver = driver
-
-    yield driver
-    driver.quit()
-
 
 def pytest_addoption(parser):
-    parser.addoption("--browser_stack_enabled", help="Execute tests on BrowserStack. Default set to 'False'.")
+    parser.addoption("--browser-stack-Enabled", help="Execute tests on BrowserStack. Default set to 'False'.")
     parser.addoption("--browser", help="Browser within which to execute tests e.g. 'Safari'.")
-    parser.addoption("--browser_version", help="Browser version to execute on e.g. '13'.")
+    parser.addoption("--browser-version", help="Browser version to execute on e.g. '13'.")
     parser.addoption("--os", help="Operating system to execute on e.g. 'OS X'.")
-    parser.addoption("--os_version", help="Operating system version e.g. 'Catalina'.")
+    parser.addoption("--os-version", help="Operating system version e.g. 'Catalina'.")
     parser.addoption("--name", help="Name for the test in the BrowserStack dashboard.")
 
 
 @pytest.fixture(scope="session")
 def browser_stack_enabled(request):
-    return request.config.getoption("--browser_stack_enabled")
+    return request.config.getoption("--browser-stack-Enabled")
 
 
 @pytest.fixture(scope="session")
@@ -100,7 +86,7 @@ def browser(request):
 
 @pytest.fixture(scope="session")
 def browser_version(request):
-    return request.config.getoption("--browser_version")
+    return request.config.getoption("--browser-version")
 
 
 @pytest.fixture(scope="session")
@@ -110,7 +96,7 @@ def os(request):
 
 @pytest.fixture(scope="session")
 def os_version(request):
-    return request.config.getoption("--os_version")
+    return request.config.getoption("--os-version")
 
 
 @pytest.fixture(scope="session")
